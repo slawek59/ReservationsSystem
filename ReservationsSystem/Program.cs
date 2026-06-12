3ad677543ff665935afd469c6d9d798e0f065d26
Slawomir Was <was.slawek@gmail.com>
2026-06-12 22:55:55 +0200

    fixed project references cleaned redundant usings

--- ReservationsSystem/Program.cs
+++ ReservationsSystem/Program.cs
@@ -9,7 +9,7 @@ using System.Text.Json.Serialization;
 
 namespace ReservationsSystem
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
