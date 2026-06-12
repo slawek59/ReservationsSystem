66f44d7215d047de0b9ab41394efd35cf655f8b0
Slawomir Was <was.slawek@gmail.com>
2026-06-13 00:59:43 +0200

    updated UpdateUserDataValidator and CreateUserDataValidator

--- ReservationsSystem.Application/Validators/User/UpdateUserDataValidator.cs
+++ ReservationsSystem.Application/Validators/User/UpdateUserDataValidator.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
+using ReservationsSystem.Application.DTOs;
 
 namespace ReservationsSystem.Application.Validators.User
 {
-	internal class UpdateUserDataValidator
+	public class UpdateUserDataValidator : AbstractValidator<UserDto>
 	{
+		public UpdateUserDataValidator()
+		{
+			RuleFor(x => x.Id)
+				.Cascade(CascadeMode.Stop)
+				.NotEmpty()
+				.WithMessage("Id is required.");
+
+			RuleFor(x => x.Email)
+				.Cascade(CascadeMode.Stop)
+				.NotEmpty()
+				.WithMessage("Email address is required.")
+				.EmailAddress()
+				.WithMessage("Email address must be a valid email format.");
+
+			RuleFor(x => x.Phone)
+				.Cascade(CascadeMode.Stop)
+				.NotEmpty()
+				.WithMessage("Phone number is required.");
+		}
 	}
 }
